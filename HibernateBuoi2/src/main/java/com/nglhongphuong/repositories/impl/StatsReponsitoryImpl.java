63743421e6b5847ff449b262304974a252c9e967
nglhongphuong <nglhongphuong@gmail.com>
2025-03-16 16:05:59 +0700

    add info stats

--- HibernateBuoi2/src/main/java/com/nglhongphuong/repositories/impl/StatsReponsitoryImpl.java
+++ HibernateBuoi2/src/main/java/com/nglhongphuong/repositories/impl/StatsReponsitoryImpl.java
@@ -4,10 +4,33 @@
  */
 package com.nglhongphuong.repositories.impl;
 
+import com.nglhongphuong.hibernatebuoi2.HibernateUtils;
+import com.nglhongphuong.pojo.Category;
+import com.nglhongphuong.pojo.OrderDetail;
+import com.nglhongphuong.pojo.Product;
+import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Root;
+import java.util.List;
+import org.hibernate.Session;
+
 /**
  *
  * @author admin
  */
 public class StatsReponsitoryImpl {
-    
+    private List<Object[]> statsRevenueByProduct(){
+         try (Session s = HibernateUtils.getFACTORY().openSession()) {
+            CriteriaBuilder b = s.getCriteriaBuilder();
+            CriteriaQuery<Object[]> q = b.createQuery(Object[].class);
+            Root root = q.from(OrderDetail.class);
+            Join<OrderDetail, Product> join = root.join("productId",JoinType.RIGHT);
+            
+            
+            return null;
+        }
+    }
 }
